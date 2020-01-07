1b324a9144b5eb9ae736f1139768457910f03d78
swapnil <ddevil691@gmail.com>
2020-01-07 20:50:04 +0530

    Setup main layout for the app

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainModule } from './modules/_main/main.module';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./modules/_main/main.module').then(m => m.MainModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/modules/_main/main-routing.module.ts
+++ frontend/src/app/modules/_main/main-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './components/main/main.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,       // Used as a layout
+    children: [
+      {
+        path: '',
+        redirectTo: 'users'
+      },
+      {
+        path: 'jobs',
+        loadChildren: () => import('../job/job.module').then(m => m.JobModule)
+      },
+      {
+        path: 'users',
+        loadChildren: () => import('../user/user.module').then(m => m.UserModule)
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
