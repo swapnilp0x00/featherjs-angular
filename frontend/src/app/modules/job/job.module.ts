49514eb68aebbe96c9b36e01156ef08fd664c2e8
swapnil <ddevil691@gmail.com>
2020-01-07 20:51:46 +0530

    Generated job list and user list components

--- frontend/src/app/modules/job/job.module.ts
+++ frontend/src/app/modules/job/job.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { JobRoutingModule } from './job-routing.module';
+import { JobListComponent } from './components/job-list/job-list.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [JobListComponent],
   imports: [
     CommonModule,
     JobRoutingModule
--- frontend/src/app/modules/user/user.module.ts
+++ frontend/src/app/modules/user/user.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
+import { UserListComponent } from './components/user-list/user-list.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [UserListComponent],
   imports: [
     CommonModule,
     UserRoutingModule
