851fc8bd4420e1b247e15b2c2d018d0ce466eb18
swapnil <ddevil691@gmail.com>
2020-01-09 23:05:58 +0530

    Added list routes for job and user

--- frontend/src/app/modules/job/job-routing.module.ts
+++ frontend/src/app/modules/job/job-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { JobListComponent } from './components/job-list/job-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: JobListComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- frontend/src/app/modules/user/user-routing.module.ts
+++ frontend/src/app/modules/user/user-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserListComponent } from './components/user-list/user-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: UserListComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
