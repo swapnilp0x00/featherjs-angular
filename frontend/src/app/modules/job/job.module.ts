e4a4f2176c5c16ea22ef4ce05c32ec48ad1e9f01
swapnil <ddevil691@gmail.com>
2020-01-09 23:40:23 +0530

    Added job form

--- frontend/src/app/modules/job/job.module.ts
+++ frontend/src/app/modules/job/job.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { JobRoutingModule } from './job-routing.module';
 import { JobListComponent } from './components/job-list/job-list.component';
+import { JobFormComponent } from './components/job-form/job-form.component';
 
 
 @NgModule({
-  declarations: [JobListComponent],
+  declarations: [JobListComponent, JobFormComponent],
   imports: [
     CommonModule,
     JobRoutingModule
