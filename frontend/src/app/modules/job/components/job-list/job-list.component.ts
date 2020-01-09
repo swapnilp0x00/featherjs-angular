4cbda491bdf4929a5d884c95a432f917f4a6371a
swapnil <ddevil691@gmail.com>
2020-01-10 00:01:57 +0530

    Added links for edit and create

--- frontend/src/app/modules/job/components/job-list/job-list.component.ts
+++ frontend/src/app/modules/job/components/job-list/job-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FeathersService } from 'src/app/shared/feathers.service';
 import { Observable, from } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { Service } from '@feathersjs/feathers';
 interface JobModel {
   id?: number;
   title: string;
@@ -16,14 +17,35 @@ interface JobModel {
 export class JobListComponent implements OnInit {
 
   jobList$: Observable<any>;
+  jobService: Service<JobModel>;
   constructor(private featherService: FeathersService) {
   }
 
   ngOnInit() {
-    const jobService = this.featherService.createService<JobModel>('job');
-    this.jobList$ = from(jobService.find()).pipe(
+    this.jobService = this.featherService.createService<JobModel>('job');
+    this.fetchAll();
+  }
+
+  fetchAll() {
+    this.jobList$ = from(this.jobService.find()).pipe(
       map((response: any) => response.data)
     );
+  }
+
+  deleteJob(job: JobModel) {
+    const response = confirm('Are you sure you want to delete this');
+    if (response) {
+      this.jobService.remove(job.id).then(() => {
+        this.fetchAll();
+        console.log('%c Job Deleted', 'font-size:20px;font-weight:bold');
+      }).catch(error => {
+        console.log('%c Error', 'color:red;font-size:20px;font-weight:bold');
+      });
+    }
+  }
+
+  openJobForm() {
+    
   }
 
 }
--- frontend/src/app/modules/job/job-routing.module.ts
+++ frontend/src/app/modules/job/job-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { JobListComponent } from './components/job-list/job-list.component';
+import { JobFormComponent } from './components/job-form/job-form.component';
 
 
 const routes: Routes = [{
   path: '',
-  component: JobListComponent
-}];
+  component: JobListComponent,
+  }, {
+  path: ':id',
+  component: JobFormComponent
+  },{
+    path: 'create',
+    component: JobFormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
