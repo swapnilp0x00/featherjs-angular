fdf3b4c1e728e1029cdcce8321b7105afb0dc217
swapnil <ddevil691@gmail.com>
2020-01-10 22:21:48 +0530

    Main component moved out of components

--- frontend/src/app/modules/_main/main.module.ts
+++ frontend/src/app/modules/_main/main.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
-import { MainComponent } from './components/main/main.component';
+import { MainComponent } from './main/main.component';
 
 
 @NgModule({
