f3212ddfa5547f838f19cd54e4e89de6626dc46c
swapnil <ddevil691@gmail.com>
2020-01-10 22:23:15 +0530

    user list moved out of components

--- frontend/src/app/modules/user/user-routing.module.ts
+++ frontend/src/app/modules/user/user-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { UserListComponent } from './components/user-list/user-list.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 
 const routes: Routes = [{
--- frontend/src/app/modules/user/user.module.ts
+++ frontend/src/app/modules/user/user.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
-import { UserListComponent } from './components/user-list/user-list.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 
 @NgModule({
