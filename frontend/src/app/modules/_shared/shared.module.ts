7be9f6395b60cfa54b1b7a413433b0d637bed369
swapnil <ddevil691@gmail.com>
2020-01-11 13:38:19 +0530

    Added form material styles to job create form

--- frontend/src/app/modules/_shared/shared.module.ts
+++ frontend/src/app/modules/_shared/shared.module.ts
@@ -5,18 +5,27 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
 import {MatTableModule} from '@angular/material/table';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     FormsModule,
     MatButtonModule,
-    MatTableModule
+    MatTableModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
   ],
   exports: [
     FormsModule,
     MatButtonModule,
-    MatTableModule
+    MatTableModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
   ]
 })
 export class SharedModule { }
